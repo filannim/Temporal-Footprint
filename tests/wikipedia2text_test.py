b9989ad443ca72830c5bc8c2e225ee6d18a287bd
Michele Filannino <filannino.m@gmail.com>
2014-07-09 18:02:18 +0100

    Added test

---
+++ tests/wikipedia2text_test.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+#
+#	Copyright 2014 Michele Filannino
+#
+#	gnTEAM, School of Computer Science, University of Manchester.
+#	All rights reserved. This program and the accompanying materials
+#	are made available under the terms of the GNU General Public License.
+#	
+#	author: Michele Filannino
+#	email:  filannim@cs.man.ac.uk
+#	
+#	For details, see www.cs.man.ac.uk/~filannim/
+
+from temporal_footprint.wikipedia2text import get_just_text
+from temporal_footprint.wikipedia2text import wikipedia_text
+
+def test_no_title():
+	assert get_just_text('','bla bla') == {'text': 'bla bla'}
+
+def test_no_raw_text():
+	assert get_just_text('a test','') == {'text': ''}
+
+def test_utf8_in_raw_text():
+	assert get_just_text('a test', 'è åß') == {'text': 'è åß'}
